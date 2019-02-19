65c8ede4948ae5ff96cde5a84a8b287d7334806c
Akzhar <akzharik@mail.ru>
2019-02-19 18:37:28 +0300

    no responsive layout

---
+++ src/js/slider.js
@@ -0,0 +1,46 @@
+var slides = document.querySelectorAll(".slide__item"); //все слайды
+var label = document.querySelectorAll(".slide__radiolabel"); //все лейблы радикнопок
+var currentSlide = 0; //счетчик слайдов
+var next = document.querySelector('.slider__btn-next'); //кнопка вперед
+var previous = document.querySelector('.slider__btn-prev'); // кнопка назад
+var slideInterval = setInterval(nextSlide,10000); //интервал повторения
+const color = "#d1e44a";
+
+function pauseSlideshow() {
+  clearInterval(slideInterval); //очистка интервала повторения
+};
+
+label[0].style.backgroundColor = color;
+
+next.onclick = function() { //при клике на вперед
+  pauseSlideshow(); //прекращение авто слайд шоу
+  nextSlide(); //вызов функции
+};
+
+previous.onclick = function() { // при клике на назад
+  pauseSlideshow(); //прекращение авто слайд шоу
+  previousSlide(); //вызов функции
+};
+
+for (var i = 0; i < label.length; i ++) {
+  label[i].onclick = function() {
+    pauseSlideshow(); //прекращение авто слайд шоу
+    goToSlide(this.id); //переход к слайду (по id input'а)
+  };
+};
+
+function nextSlide() {
+ goToSlide(currentSlide+1); //вызов функции и передача в нее № следующего слайда
+};
+
+function previousSlide() {
+ goToSlide(currentSlide-1); //вызов функции и передача в нее № предыдущего слайда
+};
+
+function goToSlide(n) {
+  label[currentSlide].style.backgroundColor=""; //снятие атрибута у текущей радио радио кнопки
+  slides[currentSlide].classList.remove("slide__item--show"); //убираем класс с текущего слайда
+  currentSlide = (n+slides.length)%slides.length; // текущий слайд -1 или +1
+  label[currentSlide].style.backgroundColor= color; //установка атрибута у текущей радио радио кнопки
+  slides[currentSlide].classList.add("slide__item--show"); //назначаем класс текущему слайду
+};
