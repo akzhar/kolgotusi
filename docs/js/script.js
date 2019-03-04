381faeeaf03df8f0c2059a3c754059b66861f577
Akzhar <akzharik@mail.ru>
2019-03-04 15:27:40 +0300

    collapse header

--- docs/js/script.js
+++ docs/js/script.js
@@ -13,11 +13,9 @@ menubutton.addEventListener("click", function(evt) {
 
 const header = document.querySelector(".header");
 const pagewrapper = document.querySelector(".page-wrapper");
-const logo = document.querySelector(".logo");
 
 header.classList.remove("header--nojs");
 pagewrapper.classList.remove("page-wrapper--nojs");
-logo.classList.remove("logo--nojs");
 
 
 const anchor = document.querySelector('.btn--up');
--- src/js/nojs.js
+++ src/js/nojs.js
@@ -1,7 +1,5 @@
 const header = document.querySelector(".header");
 const pagewrapper = document.querySelector(".page-wrapper");
-const logo = document.querySelector(".logo");
 
 header.classList.remove("header--nojs");
 pagewrapper.classList.remove("page-wrapper--nojs");
-logo.classList.remove("logo--nojs");
